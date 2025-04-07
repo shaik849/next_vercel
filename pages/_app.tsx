a36c8ae523f8fb0c0cb61fbaf95bc5aabfa13cb5
shaik849 <sanavullashaik0@gmail.com>
2025-04-07 15:36:32 +0530

    type issue fixed in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import { SessionProvider } from "next-auth/react";
-import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import Navbar from "@/components/Navbar";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
+// Define the type for pageProps including session
+interface CustomAppProps extends AppProps {
+  pageProps: {
+    session?: any; // Temporary; we'll refine this
+  };
+}
+
+export default function App({ Component, pageProps: { session, ...pageProps } }: CustomAppProps) {
   return (
     <SessionProvider session={session}>
       <Navbar />
